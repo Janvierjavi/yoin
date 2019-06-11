7502a76dbae64516441e751be625b6b5f23225f3
yoyoyoheyheyhey <asokottedotti@icloud.com>
2019-06-11 13:24:37 +0900

    add: 投稿機能
    5・7・５用の制限や体裁は未実装

--- app/controllers/senryus_controller.rb
+++ app/controllers/senryus_controller.rb
@@ -1,7 +1,27 @@
 class SenryusController < ApplicationController
   def index
+    @senryus = Senryu.all
   end
 
   def new
+    @senryu = Senryu.new
+  end
+
+  def create
+    @senryu = Senryu.new(senryu_params)
+    if @senryu.save
+      redirect_to senryus_url, notice: "posted successfully"
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def senryu_params
+    params.require(:senryu).permit(:content)
   end
 end
