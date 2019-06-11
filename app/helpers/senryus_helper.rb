860611d73dd82fb2bec874351d2deee7c3a8df51
yoyoyoheyheyhey <asokottedotti@icloud.com>
2019-06-11 15:52:15 +0900

    clean: 可読性向上をねらい、各頭文字ごとのメソッド定義

--- app/helpers/senryus_helper.rb
+++ app/helpers/senryus_helper.rb
@@ -5,4 +5,16 @@ module SenryusHelper
     initial_array = hiragana - not_want
     initial_array.sample(3)
   end
+
+  def first_initial
+    three_random_initials[0]
+  end
+
+  def second_initial
+    three_random_initials[1]
+  end
+
+  def third_initial
+    three_random_initials[2]
+  end
 end
