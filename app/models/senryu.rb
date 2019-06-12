ed40fa6cf2c298cfd07f1604b68ab5d037a92e8b
yoyoyoheyheyhey <asokottedotti@icloud.com>
2019-06-12 13:18:41 +0900

    add: 五七五の文字数バリデーションを設定

--- app/models/senryu.rb
+++ app/models/senryu.rb
@@ -1,2 +1,8 @@
 class Senryu < ApplicationRecord
+  with_options length: { is: 5 } do
+    validates :first_line
+    validates :third_line
+  end
+  validates :second_line, length: { is: 7 } 
+  
 end
