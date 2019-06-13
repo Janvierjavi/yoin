1752708d06ac8038b02fa3a9d177e93841305d57
yoyoyoheyheyhey <asokottedotti@icloud.com>
2019-06-13 12:27:22 +0900

    add: 捨て仮名が含まれていれば、捨て仮名の数分、既定の文字数制限から増やすカスタムバリデーション
    俳句のルールとして、小さい文字(捨て仮名)は前の字と合わせて一文字とするから

--- app/models/senryu.rb
+++ app/models/senryu.rb
@@ -1,8 +1,47 @@
 class Senryu < ApplicationRecord
-  with_options length: { is: 5 } do
-    validates :first_line
-    validates :third_line
+  validate :first_line_length
+  validate :second_line_length
+  validate :third_line_length
+
+  SUTEGANA = ["ぁ", "ぃ", "ぅ", "ぇ", "ぉ", "ゃ", "ゅ", "ょ"]
+  SUTEGANA.freeze
+
+  def include_sutegana?(string)
+    SUTEGANA.any? { |i| string.split("").include?(i) }
   end
-  validates :second_line, length: { is: 7 } 
-  
+
+  def sutegana_length(string)
+    arr = []
+    string.split("").each do |i|
+      if SUTEGANA.include?(i)
+        arr << i
+      end
+    end
+    arr.length
+  end
+
+  def first_line_length
+    if include_sutegana?(first_line)
+      errors.add(:first_line, "is the wrong length (should be #{5 + sutegana_length(first_line)} characters)") if first_line.length != (5 + sutegana_length(first_line))
+    else
+      errors.add(:first_line, '5 + sutegana_length(first_line)') if first_line.length != 5
+    end
+  end
+
+  def second_line_length
+    if include_sutegana?(second_line)
+      errors.add(:second_line, "is the wrong length (should be #{7 + sutegana_length(second_line)} characters)") if second_line.length != (7 + sutegana_length(second_line))
+    else
+      errors.add(:second_line, '5 + sutegana_length(second_line)') if second_line.length != 7
+    end
+  end
+
+  def third_line_length
+    if include_sutegana?(third_line)
+      errors.add(:third_line, "is the wrong length (should be #{5 + sutegana_length(third_line)} characters)") if third_line.length != (5 + sutegana_length(third_line))
+    else
+      errors.add(:third_line, '5 + sutegana_length(third_line)') if third_line.length != 5
+    end
+  end
+
 end
