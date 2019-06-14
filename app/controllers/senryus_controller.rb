363c67afbd13c48989ce861b1af58ed9759c9f79
yoyoyoheyheyhey <asokottedotti@icloud.com>
2019-06-14 18:12:20 +0900

    add: 投稿エラーでnewに戻る際、元々のランダム頭文字と入力した値を保持する
    エラーで戻るたびにランダム頭文字が全てリセットされてしまうのは、フレンドリーではないと考えたため

--- app/controllers/senryus_controller.rb
+++ app/controllers/senryus_controller.rb
@@ -4,7 +4,16 @@ class SenryusController < ApplicationController
   end
 
   def new
-    @senryu = Senryu.new
+    if params[:senryu].present?
+      @senryu_with_errors = Senryu.create(senryu_content)
+      @senryu = Senryu.new(senryu_content_without_initial)
+      
+      @first_initial = params[:senryu][:first_initial]
+      @second_initial = params[:senryu][:second_initial]
+      @third_initial = params[:senryu][:third_initial]
+    else
+      @senryu = Senryu.new
+    end
   end
 
   def create
@@ -12,7 +21,7 @@ class SenryusController < ApplicationController
     if @senryu.save
       redirect_to senryus_url, notice: "posted successfully"
     else
-      render 'new'
+      redirect_to new_senryu_url(senryu: senryu_params)
     end
   end
 
@@ -33,6 +42,14 @@ class SenryusController < ApplicationController
       "first_line": senryu_params[:first_initial] + senryu_params[:first_line], 
       "second_line": senryu_params[:second_initial] + senryu_params[:second_line], 
       "third_line": senryu_params[:third_initial] + senryu_params[:third_line]
+    }
+  end
+
+  def senryu_content_without_initial
+    {
+      "first_line": senryu_params[:first_line], 
+      "second_line": senryu_params[:second_line], 
+      "third_line": senryu_params[:third_line]
     }
   end
 
