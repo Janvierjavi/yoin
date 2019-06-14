e8f004949d6985fba928274dac07ad4408003fd4
yoyoyoheyheyhey <asokottedotti@icloud.com>
2019-06-14 17:04:16 +0900

    add: ひらがな以外の入力を受けつけないカスタムバリデーション

--- app/models/senryu.rb
+++ app/models/senryu.rb
@@ -2,9 +2,12 @@ class Senryu < ApplicationRecord
   validate :first_line_length
   validate :second_line_length
   validate :third_line_length
+  validate :other_than_hiragana
 
   SUTEGANA = ["ぁ", "ぃ", "ぅ", "ぇ", "ぉ", "ゃ", "ゅ", "ょ"]
   SUTEGANA.freeze
+
+  private
 
   def include_sutegana?(string)
     SUTEGANA.any? { |i| string.split("").include?(i) }
@@ -20,11 +23,15 @@ class Senryu < ApplicationRecord
     arr.length
   end
 
+  def hiragana?(string)
+    nil != (string =~ /\A[\u3041-\u3096]+\z/)
+  end
+
   def first_line_length
     if include_sutegana?(first_line)
       errors.add(:first_line, "is the wrong length (should be #{5 + sutegana_length(first_line)} characters)") if first_line.length != (5 + sutegana_length(first_line))
     else
-      errors.add(:first_line, '5 + sutegana_length(first_line)') if first_line.length != 5
+      errors.add(:first_line, 'is the wrong length (should be 5)') if first_line.length != 5
     end
   end
 
@@ -32,7 +39,7 @@ class Senryu < ApplicationRecord
     if include_sutegana?(second_line)
       errors.add(:second_line, "is the wrong length (should be #{7 + sutegana_length(second_line)} characters)") if second_line.length != (7 + sutegana_length(second_line))
     else
-      errors.add(:second_line, '5 + sutegana_length(second_line)') if second_line.length != 7
+      errors.add(:second_line, 'is the wrong length (should be 7)') if second_line.length != 7
     end
   end
 
@@ -40,7 +47,13 @@ class Senryu < ApplicationRecord
     if include_sutegana?(third_line)
       errors.add(:third_line, "is the wrong length (should be #{5 + sutegana_length(third_line)} characters)") if third_line.length != (5 + sutegana_length(third_line))
     else
-      errors.add(:third_line, '5 + sutegana_length(third_line)') if third_line.length != 5
+      errors.add(:third_line, 'is the wrong length (should be 5)') if third_line.length != 5
+    end
+  end
+
+  def other_than_hiragana
+    unless hiragana?(first_line + second_line + third_line)
+      errors[:base] << "ひらがな以外の文字があるため無効です"
     end
   end
 
