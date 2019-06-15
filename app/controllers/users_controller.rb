8cf5ed2dff14316eba853c381ceefaed6723eb0a
yoyoyoheyheyhey <asokottedotti@icloud.com>
2019-06-15 11:59:15 +0900

    add: ユーザー登録機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      # とりあえずリダイレクト
+      redirect_to senryus_url
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -16,4 +24,11 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :icon, :bio)
+  end
+
 end
