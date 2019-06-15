8465aa22484eeae1f6d383dc63b6e2e1bd199aa2
yoyoyoheyheyhey <asokottedotti@icloud.com>
2019-06-15 12:31:25 +0900

    add: ログイン機能

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+        session[:user_id] = @user.id
+        # とりあえずユーザーページにリダイレクト（あとで変更する）
+        redirect_to user_url(@user.id), notice: "ログインに成功しました"
+    else
+        flash[:notice] = 'ログインに失敗しました'
+        render 'new'
+    end
   end
 
   def destroy
