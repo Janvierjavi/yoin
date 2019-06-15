5198c13fb9bcff95cf70704829625234f4cfa337
yoyoyoheyheyhey <asokottedotti@icloud.com>
2019-06-15 11:37:53 +0900

    add: データベースレベルでEメールのユニークを保証するためのインデックス

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_15_021122) do
+ActiveRecord::Schema.define(version: 2019_06_15_021448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_06_15_021122) do
     t.string "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
