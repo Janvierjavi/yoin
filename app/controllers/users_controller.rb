e654000ffa01d4270b671603c696dc6773b38140
yoyoyoheyheyhey <asokottedotti@icloud.com>
2019-06-15 17:45:09 +0900

    fix: フォロー/フォロワー機能を実装するにあたって他人のユーザーページは見れるようにする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :login_required, only: %i[new create]
-  before_action :set_user, only: %i[show edit update destroy]
+  before_action :set_user, only: %i[edit update destroy]
   
   def new
     if logged_in?
@@ -33,6 +33,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def destroy
