fd7d2504ac7cff7af0c24863bdd88e7411336793
yoyoyoheyheyhey <asokottedotti@icloud.com>
2019-06-17 17:33:47 +0900

    change: favoritesのルーティングをsenryusにネスと構造

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: %w(new create edit update show destroy)
   resources :sessions, only: %w(new create destroy)
-  resources :senryus, only: %w(index new create destroy)
-  resources :favorites, only: %w(index create destroy)
+  resources :senryus, only: %w(index new create destroy) do
+    resources :favorites, only: %w(index create destroy)
+  end
 end
