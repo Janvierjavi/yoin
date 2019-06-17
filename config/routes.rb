6dbc1e7ed52803677a08ecd3098136e6b9ec08c6
yoyoyoheyheyhey <asokottedotti@icloud.com>
2019-06-17 17:09:43 +0900

    add: お気に入り機能で使用するルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :users, only: %w(new create edit update show destroy)
   resources :sessions, only: %w(new create destroy)
   resources :senryus, only: %w(index new create destroy)
+  resources :favorites, only: %w(index create destroy)
 end
