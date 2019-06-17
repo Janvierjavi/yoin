b59eddf967e548d88688fd0412aa9d6824f870e0
yoyoyoheyheyhey <asokottedotti@icloud.com>
2019-06-17 17:59:28 +0900

    add: いいね、いいね取り消しアクションの設定

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,26 @@
 class FavoritesController < ApplicationController
+  before_action :set_senryu
+
   def index
   end
 
   def create
+    @favorite = Favorite.create(user_id: current_user.id, senryu_id: params[:senryu_id])
+    @favorites = Favorite.where(senryu_id: params[:senryu_id])
+    @senryu.reload
   end
 
   def destroy
+    favorite = Favorite.find_by(user_id: current_user.id, senryu_id: params[:senryu_id])
+    favorite.destroy
+    @favorites = Favorite.where(senryu_id: params[:senryu_id])
+    @senryu.reload
   end
+
+  private
+
+  def set_senryu
+    @senryu = Senryu.find(params[:senryu_id])
+  end
+
 end
