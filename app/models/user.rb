08f90bd6e3d2b0787cb67dddcb73ad6fc86b24e8
yoyoyoheyheyhey <asokottedotti@icloud.com>
2019-06-18 19:01:12 +0900

    add: 中間テーブルを介してフォローしている/されているユーザーの情報を取得できる

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :active_relationship, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
   has_many :passive_relationship, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
+  has_many :following, through: :active_relationship, source: :followed
+  has_many :followers, through: :passive_relationship, source: :follower
 
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
