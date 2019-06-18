25fe8ed6dd30ce70fe568ee52fe818c9e92f0ad9
yoyoyoheyheyhey <asokottedotti@icloud.com>
2019-06-18 23:16:07 +0900

    clean: reloadメソッドの不要

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,14 +4,10 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    current_user.reload
-    @user.reload
   end
 
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow(@user)
-    current_user.reload
-    @user.reload
   end
 end
