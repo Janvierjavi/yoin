0a719119b588cce958e7a3f30087a6094bf22a7a
yoyoyoheyheyhey <asokottedotti@icloud.com>
2019-06-18 11:36:44 +0900

    clean: senryu.favorites_countでいいね数を表示させることができるため不要

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,14 +6,14 @@ class FavoritesController < ApplicationController
 
   def create
     @favorite = Favorite.create(user_id: current_user.id, senryu_id: params[:senryu_id])
-    @favorites = Favorite.where(senryu_id: params[:senryu_id])
+    # @favorites = Favorite.where(senryu_id: params[:senryu_id])
     @senryu.reload
   end
 
   def destroy
     favorite = Favorite.find_by(user_id: current_user.id, senryu_id: params[:senryu_id])
     favorite.destroy
-    @favorites = Favorite.where(senryu_id: params[:senryu_id])
+    # @favorites = Favorite.where(senryu_id: params[:senryu_id])
     @senryu.reload
   end
 
