46dc5c2a7951ef4df6e5f4a6e6d6120ae0bac614
yoyoyoheyheyhey <asokottedotti@icloud.com>
2019-06-18 22:58:35 +0900

    add: フォロー/フォロー解除機能のアクションを定義

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,17 @@
 class RelationshipsController < ApplicationController
+  respond_to? :js
+
+  def create
+    @user = User.find(params[:relationship][:followed_id])
+    current_user.follow!(@user)
+    current_user.reload
+    @user.reload
+  end
+
+  def destroy
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user)
+    current_user.reload
+    @user.reload
+  end
 end
