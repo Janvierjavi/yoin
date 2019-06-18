85b9c202c7699888f5b46bb69de0dae188dbc5ff
yoyoyoheyheyhey <asokottedotti@icloud.com>
2019-06-18 23:13:24 +0900

    add: Relationshipのユニーク制限が正しく機能していることを保証するSpecを追加

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user1 = FactoryBot.create(:user)
+    @user2 = FactoryBot.create(:user_second)
+    Relationship.create(follower_id: @user1.id, followed_id: @user2.id)
+  end
+
+  it "同じ人をフォローできない(Uniqueインデックスが正しく機能する)" do
+    expect do
+      Relationship.create(follower_id: @user1.id, followed_id: @user2.id)
+    end.to raise_error( ActiveRecord::RecordNotUnique )
+  end
 end
