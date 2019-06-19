db04261d61ab7ab6216f8c80b8a465663ba53ce1
yoyoyoheyheyhey <asokottedotti@icloud.com>
2019-06-19 17:34:19 +0900

    clean: 不要なインスタンス変数

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,7 @@ class FavoritesController < ApplicationController
   before_action :set_senryu
 
   def create
-    @favorite = Favorite.create(user_id: current_user.id, senryu_id: params[:senryu_id])
+    Favorite.create(user_id: current_user.id, senryu_id: params[:senryu_id])
     @senryu.reload
   end
 
