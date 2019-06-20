bd10e8c29351c04b598d205b45818f7973f69788
yoyoyoheyheyhey <asokottedotti@icloud.com>
2019-06-20 17:05:51 +0900

    add: 自分自身をフォローできないカスタムバリデーション

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,12 @@
 class Relationship < ApplicationRecord
   belongs_to :followed, class_name: 'User'
   belongs_to :follower, class_name: 'User'
+
+  validate :follow_myself
+
+  private
+
+  def follow_myself
+    errors[:base] << "自分自身をフォローすることはできません" if follower_id == followed_id
+  end
 end
