8fb667f35915823cbcbc9201b2347b6f1484ff32
yoyoyoheyheyhey <asokottedotti@icloud.com>
2019-06-20 15:51:13 +0900

    add: ポルターガイストの設定

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 
+require 'capybara/poltergeist'
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
@@ -21,6 +22,8 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   
   config.filter_rails_from_backtrace!
+
+  Capybara.javascript_driver = :poltergeist
   
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
