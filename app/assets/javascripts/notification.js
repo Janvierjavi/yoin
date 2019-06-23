dfad2196e971c34dfe679eb2dcd96017b5d3e27f
yoyoyoheyheyhey <asokottedotti@icloud.com>
2019-06-23 23:02:54 +0900

    add: 投稿ページでのエラーメッセージ フロント

--- app/assets/javascripts/notification.js
+++ app/assets/javascripts/notification.js
@@ -1,5 +1,15 @@
 $(window).on("load", function() {
   setTimeout(function() {
-    $(".footer").fadeOut("slow");
+    $(".notification-footer").fadeOut("slow");
   }, 3000);
+
+  $(".sutegana-rule").click(function() {
+    $(".sutegana-rule-explanation").addClass("open");
+    $(".general-container").addClass("dark");
+  });
+
+  $(".close-sutegana-rule-explanation").click(function() {
+    $(".sutegana-rule-explanation").removeClass("open");
+    $(".general-container").removeClass("dark");
+  });
 });
