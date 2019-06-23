d2546d936558449b149b928115ef89498a319fde
yoyoyoheyheyhey <asokottedotti@icloud.com>
2019-06-24 08:58:05 +0900

    clean: notification関係のフロント記述、ファイル分類を明確化

--- app/assets/javascripts/notification.js
+++ app/assets/javascripts/notification.js
@@ -2,14 +2,4 @@ $(window).on("load", function() {
   setTimeout(function() {
     $(".notification-footer").fadeOut("slow");
   }, 3000);
-
-  $(".sutegana-rule").click(function() {
-    $(".sutegana-rule-explanation").addClass("open");
-    $(".general-container").addClass("dark");
-  });
-
-  $(".close-sutegana-rule-explanation").click(function() {
-    $(".sutegana-rule-explanation").removeClass("open");
-    $(".general-container").removeClass("dark");
-  });
 });
